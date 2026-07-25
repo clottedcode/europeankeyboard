81522c0aab7f08d9d97aa33521de4a8c703951b4
Ronan Bakker <rbkkr95@gmail.com>
2026-07-25 16:06:05 +0200

    Delete samenvoegen.py

--- samenvoegen.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfWriter
-
-merger = PdfWriter()
-
-for pdf in ["Hadisch versie 2b.pdf", "Hadisch.pdf"]:
-    merger.append(pdf)
-
-merger.write("Hadisch Uitleg.pdf")
-merger.close()
